ba45b400db7a87e023121001c0988bb5c2cb871e
fagai <fg.fagai@gmail.com>
2012-11-28 22:13:45 +0900

    add

---
+++ app.js
@@ -0,0 +1,49 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path')
+  , io = require('socket.io');
+
+var app = express();
+var server = http.createServer(app);
+var io = io.listen(server);
+
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('your secret here'));
+  app.use(express.session());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+server.listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+io.sockets.on('connection', function(socket){
+  socket.on('msg post', function(msg) {
+    console.log(msg);
+    socket.emit('msg push', msg);
+    socket.broadcast.emit('msg push', msg);
+  });
+});
